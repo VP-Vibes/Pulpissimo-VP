085b7130906c54920082dac6f7875fd1585a2253
ga38qoh <uzair.sharif@tum.de>
2022-10-06 15:31:41 +0200

    some more cleanup on -1

--- src/tgc_vp/system.h
+++ src/tgc_vp/system.h
@@ -35,8 +35,6 @@ namespace tgc_vp {
 
 class system : public sc_core::sc_module {
  public:
-  SC_HAS_PROCESS(system);  // NOLINT
-
   sc_core::sc_in<bool> erst_n{"erst_n"};
 
   system(sc_core::sc_module_name nm);
@@ -44,33 +42,31 @@ class system : public sc_core::sc_module {
  private:
   sysc::tgfs::core_complex core_complex{"core_complex"};
   scc::router<> router;
+  // TODO: uart0 is kept for debugging in dev for now..later remove this
   vpvper::sifive::uart_terminal uart0{"uart0"};
   vpvper::sifive::plic plic{"plic"};
   vpvper::sifive::aon aon{"aon"};
   vpvper::sifive::prci prci{"prci"};
   vpvper::sifive::clint clint{"clint"};
-
-  //  Dummy dummy_{};
   vpvper::pulpissimo::udma udma{"udma"};
-
-  using mem_qspi_t = scc::memory<512_MB, 32>;
-  mem_qspi_t mem_qspi{"mem_qspi"};
-  using mem_ram_t = scc::memory<128_kB, 32>;
-  mem_ram_t mem_ram{"mem_ram"};
-
+  scc::memory<512_MB, 32> mem_qspi{"mem_qspi"};
+  scc::memory<128_kB, 32> mem_ram{"mem_ram"};
   sc_core::sc_signal<sc_core::sc_time, sc_core::SC_MANY_WRITERS> tlclk_s{"tlclk_s"};
   sc_core::sc_signal<sc_core::sc_time, sc_core::SC_MANY_WRITERS> lfclk_s{"lfclk_s"};
-
   sc_core::sc_signal<bool, sc_core::SC_MANY_WRITERS> rst_s{"rst_s"}, mtime_int_s{"mtime_int_s"},
       msie_int_s{"msie_int_s"};
-
   sc_core::sc_vector<sc_core::sc_signal<bool, sc_core::SC_MANY_WRITERS>> global_int_s{"global_int_s", 256},
       local_int_s{"local_int_s", 16};
   sc_core::sc_signal<bool, sc_core::SC_MANY_WRITERS> core_int_s{"core_int_s"};
 
  protected:
+  const std::array<scc::target_memory_map_entry<32>, 5> platfrom_mmap = {{{clint.socket, 0x2000000, 0xc000},
+                                                                          {plic.socket, 0xc000000, 0x200008},
+                                                                          {aon.socket, 0x10000000, 0x150},
+                                                                          {prci.socket, 0x10008000, 0x14},
+                                                                          {uart0.socket, 0x10013000, 0x1c}}};
+
   void gen_reset();
-#include "tgc_vp/gen/platform_mmap.h"
 };
 
 }  // namespace tgc_vp
