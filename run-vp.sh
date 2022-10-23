2c4be98f151b4e6538a222922718b42800d2c9be
uzleo <uzleo_eth@proton.me>
2022-10-23 16:20:39 +0000

    amend for -1

--- run-vp.sh
+++ run-vp.sh
@@ -1,19 +1,16 @@
 #!/bin/sh
 
-# if you want to compile VP first time
-#mkdir -p build/
-#cd build/
-#cmake -DCMAKE_BUILD_TYPE=Release ..
-#make -j4 tgc-vp
 
 # if you want to compile fw
-#cd ../fw/udma_spim_tests
+#cd fw/udma_spim_tests
 #mkdir -p build/obj/
 #make
+#cd ../../
 
 # if build and run vp simulation
 FW_TO_RUN=../fw/udma_spim_tests/build/test
 
 cd build/
+cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j4 tgc-vp
 src/tgc-vp -f ${FW_TO_RUN}
