95b9e125b57a5065824e1bc6ddf0f76a617bd881
uzleo <uzleo_eth@proton.me>
2022-11-02 17:00:29 +0000

    wip

--- fw/udma_spim_tests/main.c
+++ fw/udma_spim_tests/main.c
@@ -6,7 +6,7 @@
 // receive data from external device using uDMA via SPIM the transfer takes place synchronously i.e. the CPU execution
 // is blocked till this function returns
 int testReceiveSync() {
-  int buffer_size = 16;
+  const int buffer_size = 16;
 
   rt_spim_conf_t conf;
   rt_spim_conf_init(&conf);
@@ -40,23 +40,48 @@ int testReceiveSync() {
 // receive data from external device using uDMA via SPIM the transfer takes place asynchronously i.e. the CPU execution
 // is not blocked till this function returns the CPU can do other stuff till an interrupt is received for end of
 // transfer by uDMA
-int testReceiveAsync() { return 0; }
+int testReceiveAsync() {
+  // TODO
+  return 0;
+}
 
-int testTransmitSync() { return 0; }
+int testTransmitSync() {
+  const int buffer_size = 16;
 
-int testTransmitAsync() { return 0; }
+  rt_spim_conf_t conf;
+  rt_spim_conf_init(&conf);
+  conf.max_baudrate = 1000000;
+  conf.id = 0;
+  conf.cs = 1;
+
+  rt_spim_t *spim = rt_spim_open(NULL, &conf, NULL);
+  if (spim == NULL) {
+    return -1;
+  }
+
+  char *tx_buffer = rt_alloc(RT_ALLOC_PERIPH, buffer_size);
+  if (tx_buffer == NULL) {
+    return -1;
+  }
+  for (int i = 0; i < buffer_size; ++i) {
+    tx_buffer[i] = i * i;
+  }
+
+  rt_spim_send(spim, tx_buffer, buffer_size * 8, RT_SPIM_CS_AUTO, NULL);
+
+  return 0;
+}
+
+int testTransmitAsync() {
+  // TODO
+  return 0;
+}
 
 int main() {
-  int status1 = testReceiveSync();
-  int status2 = testReceiveAsync();
+  // int status1 = testReceiveSync();
+  // int status2 = testReceiveAsync();
   int status3 = testTransmitSync();
-  int status4 = testTransmitAsync();
-
-  if (status1 == 0) {
-    *(volatile int *)(0x1a104074) = 0x45;
-  } else if (status1 == -1) {
-    *(volatile int *)(0x1a104074) = 0x23;
-  }
+  // int status4 = testTransmitAsync();
 
   return 0;
 }
