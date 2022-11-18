f07cddd90d8c096fcdab1c0464562be5a59d4aeb
uzleo <uzleo.sharif@gmail.com>
2022-11-18 12:04:46 +0000

    org

--- run-vp.sh
+++ run-vp.sh
@@ -9,24 +9,23 @@ export PULP_SDK_HOME=/Pulpissimo-VP/fw/custom-pulp-sdk-master/
 # cp conan.cmake build/.
 # cd build/
 # cmake -DCMAKE_BUILD_TYPE=Release ..
-# cd ../
+# cd /Pulpissimo-VP/
 #
 #
 # # if you want to compile SDK
 # cd fw/custom-pulp-sdk-master/
 # make clean
 # make all
-# cd ../../
+# cd /Pulpissimo-VP/
 
 
-
-# # if you want to compile fw
-# cd fw/udma_spim_tests/
-# make clean
-# mkdir -p build/obj/
-# make
-# /opt/riscv/bin/riscv32-unknown-elf-objdump -d build/test > build/test.dump
-# cd ../../
+# if you want to compile fw
+cd fw/i2s/test/
+make clean
+mkdir -p build/obj/
+make
+/opt/riscv/bin/riscv32-unknown-elf-objdump -d build/i2s > build/i2s.dump
+cd /Pulpissimo-VP/
 
 
 
@@ -34,7 +33,7 @@ export PULP_SDK_HOME=/Pulpissimo-VP/fw/custom-pulp-sdk-master/
 
 
 # if build and run vp simulation
-FW_TO_RUN=../fw/udma_spim_tests/build/test
+FW_TO_RUN=/Pulpissimo-VP/fw/i2s/test/build/i2s
 
 cd build/
 make -j4 tgc-vp
